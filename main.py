9e05527c2a4f42bc316e8c980cb1bf84715a0cae
Abdulft01 <abdulraheemft01@gmail.com>
2025-12-24 21:22:10 +0530

    Phase 4: Added pipes, collision handling, score and difficulty scaling

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+import random
 
 # init pygame
 pygame.init()
@@ -13,7 +14,7 @@ pygame.display.set_caption("Flappy Bird | Abdul")
 clock = pygame.time.Clock()
 
 font = pygame.font.SysFont(None, 48)
-restart_font = pygame.font.SysFont(None, 30)
+score_font = pygame.font.SysFont(None, 30)
 
 # bird
 bird_x = 80
@@ -24,8 +25,37 @@ gravity = 0.5
 bird_velocity = 0
 jump_force = -8
 
+# pipes
+pipe_width = 60
+pipe_gap = 150
+pipe_speed = 3
+pipes = []
+
+SPWANPIPE = pygame.USEREVENT
+pygame.time.set_timer(SPWANPIPE, 1500) # pipe spawn every 1.5 seconds that helps bird to adjust to pass the next pipe
+
 # game state
 game_active = True
+# score
+score = 0
+
+# fun to create pipes
+def create_pipe():
+    height = random.randint(150, 450)
+    top_pipe = pygame.Rect(WIDTH, 0, pipe_width, height - pipe_gap // 2)
+    bottom_pipe = pygame.Rect(WIDTH, height + pipe_gap // 2, pipe_width, HEIGHT - (height + pipe_gap // 2))
+
+    return {"top": top_pipe, "bottom": bottom_pipe, "passed": False}
+
+# fun to reset the game
+def reset_game():
+    global bird_y, bird_velocity, pipes, score, pipe_speed, game_active
+    bird_y = 300
+    bird_velocity = 0
+    pipes = []
+    score = 0
+    pipe_speed = 3
+    game_active = True
 
 # game loop
 running = True
@@ -40,9 +70,10 @@ while running:
                 bird_velocity = jump_force
 
             if event.key == pygame.K_r and not game_active:
-                bird_y = 300
-                bird_velocity = 0
-                game_active = True
+                reset_game()
+
+        if event.type == SPWANPIPE and game_active:
+            pipes.append(create_pipe())
 
     # game logic
     if game_active:
@@ -58,16 +89,49 @@ while running:
         if bird_y + bird_radius >= HEIGHT:
             game_active = False
 
-    screen.fill((135, 206, 235)) # sky blue background of window
+        # moving the pipes
+        for pipe in pipes:
+            pipe["top"].x -= pipe_speed
+            pipe["bottom"].x -= pipe_speed
 
-    # bird design
+        pipes = [pipe for pipe in pipes if pipe["top"].right > 0]
+
+
+        bird_rect = pygame.Rect(bird_x - bird_radius, bird_y - bird_radius, bird_radius * 2, bird_radius *2)
+
+        for pipe in pipes:
+            # collision
+            if bird_rect.colliderect(pipe["top"]) or bird_rect.colliderect(pipe["bottom"]):
+                game_active = False
+
+            # scoring
+            if pipe["top"].right < bird_x and not pipe["passed"]:
+                pipe["passed"]= True
+                score += 1
+
+                if score % 5 == 0:
+                    pipe_speed += 0.5
+
+    screen.fill((135, 206, 235)) # sky blue background color 
+
+    # drawing pipe
+    for pipe in pipes:
+        pygame.draw.rect(screen, (0, 200, 0), pipe["top"])
+        pygame.draw.rect(screen, (0, 200, 0), pipe["bottom"])
+
+    # drawing bird
     pygame.draw.circle(screen, (255, 255, 0), (bird_x, int(bird_y)), bird_radius)
+
+    # score display
+    score_text = score_font.render(f"Score: {score}", True, (0, 0, 0))
+    screen.blit(score_text, (10, 10))
+
 
     if not game_active:
         game_over_text = font.render("Game Over :)", True, (255, 0, 0))
-        game_restart_text = restart_font.render("Press R to Restart the Game", True, (0, 0, 0))
+        restart_text = score_font.render("Press R to Restart the Game", True, (0, 0, 0))
         screen.blit(game_over_text, (WIDTH // 2 - 120, HEIGHT // 2 - 30))
-        screen.blit(game_restart_text, (WIDTH // 2 - 130, HEIGHT // 2 + 10))
+        screen.blit(restart_text, (WIDTH // 2 - 130, HEIGHT // 2 + 10))
 
 
     pygame.display.update()
