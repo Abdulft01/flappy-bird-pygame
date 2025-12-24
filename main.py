b794f7691fbe2d54507be8ebee4a5b9b97fc7b2c
Abdulft01 <abdulraheemft01@gmail.com>
2025-12-24 17:02:41 +0530

    Phase 2: Added bird design along with game physics that includes gravity and jump

--- main.py
+++ main.py
@@ -12,6 +12,15 @@ pygame.display.set_caption("Flappy Bird | Abdul")
 
 clock = pygame.time.Clock()
 
+# bird
+bird_x = 80
+bird_y = 300
+bird_radius = 15
+
+gravity = 0.5
+bird_velocity = 0
+jump_force = -8
+
 # game loop
 running = True
 while running:
@@ -20,7 +29,19 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                bird_velocity = jump_force
+
+    # gravity effect
+    bird_velocity += gravity
+    bird_y += bird_velocity
+
     screen.fill((135, 206, 235)) # sky blue background of window
+
+    # bird design
+    pygame.draw.circle(screen, (255, 255, 0), (bird_x, int(bird_y)), bird_radius)
+
 
     pygame.display.update()
 
